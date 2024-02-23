f4dc06a854235c7e6c22c76886bdf1a821f13e3f
asimalizamurani <asimalizamurani@gmail.com>
2024-02-23 09:16:51 +0500

    process spelling were not right.

--- backend-6/src/index.js
+++ backend-6/src/index.js
@@ -1,45 +1,20 @@
-// require('dotenv').config({path: './env'});
-import dotenv from "dotenv";
-import connectDB from "./db/index.js";
-
-dotenv.config({
-    path: './env'
-})
-
-
-connectDB();
-
-
-
-
-
-
-
-
-
-
-// First way to connect it to the database.
-
-/*
 import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import {DB_NAME} from "./constants";
 
 import express from "express";
 const app = express();
+// function connectDB (){}
+
+// connectDB();
 
 ( async () => {
-    try {
-       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-       app.on("error", (error) => {
-        console.log("ERROR", error);
-        throw error;
+    try{
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}}`)
+       app.on("error", () => {
+        console.log("ERROR", error); 
        })
-
-       app.listen(process.env.PORT, () => {
-        console.log(`Server is running on port ${process.env.PORT}`);
-       })
-    } catch (error) {
-        console.error("Error: ", error);
+    } catch(error) {
+        console.error("ERROR", error)
+        throw err
     }
-})()
-*/
+}) ()
